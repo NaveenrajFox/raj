19f6a6d5b067529b7e9747dff44dd1f0320f45d7
NaveenrajFox <37070238+NaveenrajFox@users.noreply.github.com>
2018-03-07 22:07:00 +0530

    Create oddoreven.cpp

---
+++ oddoreven.cpp
@@ -0,0 +1,18 @@
+#include <iostream.h>
+#include <conio.h>
+void main()
+{
+int a;
+clrscr();
+cout << "enter as integer" ;
+cin >> a;
+if ( a%2==0 )
+{
+cout << "the number is even";
+}
+else
+{
+cout << "the number is odd";
+}
+getch();
+}
