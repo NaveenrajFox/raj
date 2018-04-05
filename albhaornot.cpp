00f1c0f73e56a3e5aa46d748274abc32fe4ecf5c
NaveenrajFox <37070238+NaveenrajFox@users.noreply.github.com>
2018-04-05 14:16:08 +0530

    Create albhaornot.cpp

---
+++ albhaornot.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+char a;
+cin >> a;
+if( a>='a' && a<='z'||a>='A' && a<='Z'){
+    cout<<"yes";
+}
+else{
+    cout<<"no";
+}
+	return 0;
+}
