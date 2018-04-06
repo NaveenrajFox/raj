8cf51e7284f2c5874321cfdef730a0727700de1a
NaveenrajFox <37070238+NaveenrajFox@users.noreply.github.com>
2018-04-06 18:55:43 +0530

    Create primeornot.cpp

---
+++ primeornot.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int num , i, count=0;
+cout << "enter the number" ;
+cin >> num ;
+for (i=2 ; i<num ; i++)
+             {
+                 if(num % i==0)
+                 {
+                     count++;
+                     break;
+                 }
+             }
+                 if (count==0)
+                 {
+                     cout<<"\nyes";
+                 }
+                 else
+                 {
+                     cout << "\nno" ;
+                 }
+	return 0;
+}
