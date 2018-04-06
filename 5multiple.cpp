3d14a7a3b6750218dbed3b6ef4cbcfaed02df020
NaveenrajFox <37070238+NaveenrajFox@users.noreply.github.com>
2018-04-06 19:14:39 +0530

    Create 5multiple.cpp

---
+++ 5multiple.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int  n;
+cout << "enter the number" ;
+cin >> n ;
+for (int i=1 ; i<=5 ; i++)
+             {
+                 cout <<"\n" <<n*i;
+             }
+                
+	return 0;
+}
